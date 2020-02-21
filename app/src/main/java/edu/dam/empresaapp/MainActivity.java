092bc1f759177f694e5d7243ae2a2f6dbefa8ff2
fsanmara <fermin.sm@gmail.com>
2020-02-21 23:46:34 +0000

    21/02/2020 _ 23:46

--- app/src/main/java/edu/dam/empresaapp/MainActivity.java
+++ app/src/main/java/edu/dam/empresaapp/MainActivity.java
@@ -2,13 +2,27 @@ package edu.dam.empresaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    //método que al pulsar en el ImageView o TextView de la cabecera
+    //nos lleva a la pantalla de Registro
+    public void signup(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/dam/empresaapp/RegistroActivity.java
+++ app/src/main/java/edu/dam/empresaapp/RegistroActivity.java
@@ -2,7 +2,10 @@ package edu.dam.empresaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    //método que al pulsar en el ImageView o TextView de la cabecera
+    //nos lleva a la pantalla de Login
+    public void login(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
