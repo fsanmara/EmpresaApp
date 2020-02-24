836a16ccbc79e97dcacd8679a965eedefdb6e774
fsanmara <fermin.sm@gmail.com>
2020-02-24 00:03:15 +0000

    24/02/2020 _ 00:02

--- app/src/main/java/edu/dam/empresaapp/PrincipalActivity.java
+++ app/src/main/java/edu/dam/empresaapp/PrincipalActivity.java
@@ -3,12 +3,19 @@ package edu.dam.empresaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    TextView tvUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        tvUser = findViewById(R.id.tvUser);
+
     }
 }
