1515d0862d8be6b0f886bfe5a657a898c32e47b7
fsanmara <fermin.sm@gmail.com>
2020-02-24 22:32:50 +0000

    24/02/2020 _ 22:33

--- app/src/main/java/edu/dam/empresaapp/PrincipalActivity.java
+++ app/src/main/java/edu/dam/empresaapp/PrincipalActivity.java
@@ -1,13 +1,31 @@
 package edu.dam.empresaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class PrincipalActivity extends AppCompatActivity {
 
+    //declaramos vistas
     TextView tvUser;
+
+    //declaramos objeto Firebase y referencia BBDD
+    FirebaseAuth mAuth;
+    DatabaseReference dbTrabajadores;
+    String nombre, formateado;
+
+
 
 
     @Override
@@ -15,7 +33,35 @@ public class PrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        //referenciamos vistas
         tvUser = findViewById(R.id.tvUser);
+
+
+        //obtenemos la instancia de FirebaseAuth para pasarla como parámetro a la instancia de la BBDD y
+        //obtener la referencia del "hijo" de "trabajadores" que esté logueado en ese momento
+        mAuth = FirebaseAuth.getInstance();
+        dbTrabajadores = FirebaseDatabase.getInstance().getReference().child("Trabajadores").child(mAuth.getUid());
+
+
+
+        dbTrabajadores.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String nombre = dataSnapshot.child("nombre").getValue().toString();
+                //String apellido1 = dataSnapshot.child("apellido1").getValue().toString();
+                //String apellido2 = dataSnapshot.child("apellido2").getValue().toString();
+                @SuppressLint("StringFormatMatches") String formateado = getString(R.string.Hola, nombre);
+
+                tvUser.setText(formateado);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
