93fe5f799e7b751f5d4e76420733a17f4891473a
fsanmara <fermin.sm@gmail.com>
2020-03-01 23:35:51 +0000

    01/03/2020 _ 23:35

--- app/src/main/java/edu/dam/empresaapp/VacacionesActivity.java
+++ app/src/main/java/edu/dam/empresaapp/VacacionesActivity.java
@@ -3,6 +3,8 @@ package edu.dam.empresaapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +24,11 @@ public class VacacionesActivity extends AppCompatActivity {
 
     //declaramos vistas
     TextView tvNombreTrabajador;
+    CardView cardSolicVacac, cardConsultVacac, cardConsultEstado;
+
+    //Instanciamos un objeto de los fragments
+    SolicitarVacacionesFragment svf = new SolicitarVacacionesFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +37,9 @@ public class VacacionesActivity extends AppCompatActivity {
 
         //referenciamos vistas
         tvNombreTrabajador = findViewById(R.id.tvNombreTrabajador_Vacaciones);
-
-
-
+        cardSolicVacac     = findViewById(R.id.cardSolicVacac);
+        cardConsultEstado  = findViewById(R.id.cardConsultEstado);
+        cardConsultVacac   = findViewById(R.id.cardConsultVacac);
 
 
         // creamos un objeto "Trabajador", cuyo contenido será el objeto
@@ -44,7 +51,20 @@ public class VacacionesActivity extends AppCompatActivity {
         //Mostramos el nombre del trabajador en un TextView
         tvNombreTrabajador.setText(trabajador.getNombre() + " " + trabajador.getApellido1());
 
+        cardSolicVacac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                // al pulsar en el CardView de "solicitar vacaciones", iniciamos
+                // la transición del Fragment, reemplazando el layout de la Activity
+                // por el layout del Fragment "SolicitarVacacionesFragment"
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.contenedor, svf);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
 
     }
 
